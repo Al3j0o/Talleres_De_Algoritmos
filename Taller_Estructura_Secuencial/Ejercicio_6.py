70e1f33eb1b0528b2a7d687cf0a0c8bc73843d3a
Alejandro Parraga <japarra00115@universidadean.edu.co>
2022-08-05 21:04:11 -0500

    Ejercicio_6

---
+++ Taller_Estructura_Secuencial/Ejercicio_6.py
@@ -0,0 +1,7 @@
+Hom=int(input("Ingrese el numero de hombres en el curso: "))
+Muj=int(input("Ingrese el numero de mujeres en el curso: "))
+Alum=Hom+Muj
+porcHom=(Hom/Alum)*100
+porcMuj=(Muj/Alum)*100
+print("el porcentaje de hombres:",porcHom,"%")
+print("El porcentaje de mujeres es:",porcMuj,"%")
