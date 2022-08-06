be9c3e3ac76393ef198dece1e06962e626dc1bd0
Alejandro Parraga <japarra00115@universidadean.edu.co>
2022-08-05 22:54:38 -0500

    Ejercicio_10

---
+++ Taller_Estructura_Secuencial/Ejercicio_10.py
@@ -0,0 +1,12 @@
+ChelAus=float(input("Ingrese la cantidad de chelines austriacos: "))
+DracGr=float(input("Ingrese la cantidad de dracmas griegos: "))
+Pes=float(input("Ingrese la cantidad de pesetas: "))
+
+Pese=956.871/100
+FrancoF=(88.607/100)/20.110
+dolar=Pes*122.499
+liras=(9.289/100)*Pes
+
+print("Los chelines austriacos en pesetas es de:",Pese)
+print("Los dracmas griegos en francos franceses es de:",FrancoF)
+print("Las pesetas en dolares es:",dolar,"y en liras italianas:",liras)
