d36b6a4f41e0f4252b615972ca04ce88e80e6047
Alejandro Parraga <japarra00115@universidadean.edu.co>
2022-08-05 21:43:39 -0500

    Ejercicio_9

---
+++ Taller_Estructura_Secuencial/Ejercicio_9.py
@@ -0,0 +1,6 @@
+Numh=int(input("Ingrese el numero de horas trabajadas: "))
+Prech=float(input("Ingrese el precio por hora: "))
+sueldo= Numh*Prech
+imp=sueldo*0.2
+sueldoT=sueldo-imp
+print("El sueldo total es:", sueldoT)
