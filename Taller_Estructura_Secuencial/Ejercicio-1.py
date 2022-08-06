386178235c025686711ba0ccdd873a56d2f05d71
Alejandro Parraga <japarra00115@universidadean.edu.co>
2022-08-05 20:27:20 -0500

    Ejercicio_1

---
+++ Taller_Estructura_Secuencial/Ejercicio-1.py
@@ -0,0 +1,5 @@
+Edad1=int(input("Ingrese la primera edad: "))
+Edad2=int(input("Ingrese la segunda edad: "))
+Edad3=int(input("Ingrese la tercera edad: "))
+promedio=(Edad1+Edad2+Edad3)/3
+print("EL promedio es: ", promedio)
