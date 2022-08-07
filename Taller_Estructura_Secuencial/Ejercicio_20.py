72f318a5a287702b6ac27795d467fe0df6d6c9d6
Alejandro Parraga <japarra00115@universidadean.edu.co>
2022-08-07 10:27:51 -0500

    Ejercicio_20

---
+++ Taller_Estructura_Secuencial/Ejercicio_20.py
@@ -0,0 +1,8 @@
+comp=float(input("Ingrese el valor total del computador: "))
+cuot=float(input("Ingrese el valor de la cuota: "))
+cuoT=cuot*12
+recar=100-((100*comp)/cuoT)
+recar2=100-((100*cuot)/comp)
+print("El porcentaje de la recarga es de:",recar,"% Total")
+print("El porcentaje de la recarga mensual es de:",recar2,"% mensual")
+
