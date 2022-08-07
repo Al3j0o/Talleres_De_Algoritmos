95bac355172208d213061119d86c2aa903e36e1a
Alejandro Parraga <japarra00115@universidadean.edu.co>
2022-08-07 09:27:06 -0500

    Ejercicio_18

---
+++ Taller_Estructura_Secuencial/Ejercicio_18.py
@@ -0,0 +1,4 @@
+Bolx=float(input("Ingrese el valor del prestamo: "))
+Boly=float(input("Ingrese la cantidad de intereses pagado: "))
+razon=(Boly*100)/(Bolx*4)
+print("El porcentaje anual es de:", razon," %")
