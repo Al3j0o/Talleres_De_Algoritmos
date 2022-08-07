3684be268cd1204f03b6103f56c351b5ae859c13
Alejandro Parraga <japarra00115@universidadean.edu.co>
2022-08-06 22:54:36 -0500

    Ejercicio_14

---
+++ Taller_Estructura_Secuencial/Ejercicio_14.py
@@ -0,0 +1,5 @@
+kilo1=float(input("Ingrese la lectura del mes anterior: "))
+kilo2=float(input("Ingrese la lectura del presente mes: "))
+costk=float(input("Ingrese el costo por kilovatio: "))
+costm=kilo2*costk
+print("El valor  a pagar este mes es de:",costm)
