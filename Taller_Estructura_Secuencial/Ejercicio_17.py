2e31016c549756977e95d6f4a51645aeea4537ce
Alejandro Parraga <japarra00115@universidadean.edu.co>
2022-08-06 23:08:11 -0500

    Ejercicio_17

---
+++ Taller_Estructura_Secuencial/Ejercicio_17.py
@@ -0,0 +1,6 @@
+presu=float(input("Ingrese el presupuesto total: "))
+presgin=presu*0.4
+prestrau=presu* 0.3
+print("El presupuesto para Ginecologia es de:",presgin)
+print("El resupuesto para traumatologia es de:",prestrau)
+print("El presupuesto para pediatria es de:",prestrau)
