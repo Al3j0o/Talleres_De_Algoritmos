e85af7c5a768a100a0eb7058e772b558650b0d3c
Alejandro Parraga <japarra00115@universidadean.edu.co>
2022-08-11 16:22:26 -0500

    Ejercicio_11

---
+++ Taller_Estuctura_Control_Selectivo/Ejercicio_11.py
@@ -0,0 +1,14 @@
+temp=float(input("Ingrese la temperatura: "))
+if(temp>85 and temp<100):
+    print("Practique natación")
+if(temp<70 and temp<=85):
+    print("Practique tenis")
+if(temp<32 and temp<=70):
+    print("Practique golf")
+if(temp<10 and temp<=32):
+    print("Practique esqui")
+if(temp>0 and temp<=10):
+    print("Practique marcha")
+if(temp<=0 and temp>=100):
+    print("FALLO")
+    
