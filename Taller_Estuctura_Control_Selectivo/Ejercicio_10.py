ca07e05f216a5415abc6347025061ecb44e0ac22
Alejandro Parraga <japarra00115@universidadean.edu.co>
2022-08-11 15:44:08 -0500

    Ejercicio_10

---
+++ Taller_Estuctura_Control_Selectivo/Ejercicio_10.py
@@ -0,0 +1,27 @@
+Categoria=int(input("Ingrese la categoria del trabajador: "))
+if(Categoria==1):
+    Sueldo=5000000
+    Incremento=0.1
+    Sueldo_t=Sueldo+(Sueldo*Incremento)
+    print("Su sueldo es de:",Sueldo_t)
+elif(Categoria==2):
+    Sueldo=4300000
+    Incremento=0.15
+    Sueldo_t=Sueldo+(Sueldo*Incremento)
+    print("Su sueldo es de:",Sueldo_t)
+elif(Categoria==3):
+    Sueldo=3600000
+    Incremento=0.2
+    Sueldo_t=Sueldo+(Sueldo*Incremento)
+    print("Su sueldo es de:",Sueldo_t)
+elif(Categoria==4):
+    Sueldo=2000000
+    Incremento=0.4
+    Sueldo_t=Sueldo+(Sueldo*Incremento)
+    print("Su sueldo es de:",Sueldo_t)
+elif(Categoria==5):
+    Sueldo=900000
+    Incremento=0.6
+    Sueldo_t=Sueldo+(Sueldo*Incremento)
+    print("Su sueldo es de:",Sueldo_t)
+    
