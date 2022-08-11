27ab8eca62b3f8ad9cdbe10f4905a134a8443d30
Alejandro Parraga <japarra00115@universidadean.edu.co>
2022-08-10 19:09:53 -0500

    Ejercicio_3

---
+++ Taller_Estuctura_Control_Selectivo/Ejercicio_3.py
@@ -0,0 +1,11 @@
+a=float(input("Ingrese a: "))
+b=float(input("Ingrese b: "))
+c=float(input("Ingrese c: "))
+d=float(input("Ingrese d: "))
+if(d==0):
+    e=(a-c)**2
+    print(e)
+elif(d>0):
+    e=((a-b)**3)/d
+else:
+    print("NO valido")
