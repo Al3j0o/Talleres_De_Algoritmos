e3fa145fa0781dc4ebc121282cedc0b3c4fde73f
Alejandro Parraga <japarra00115@universidadean.edu.co>
2022-08-10 19:23:05 -0500

    Ejercicio_8

---
+++ Taller_Estuctura_Control_Selectivo/Ejercicio_8.py
@@ -0,0 +1,9 @@
+p=int(input("Ingrese P: "))
+q=int(input("Ingrese Q: "))
+l=(p**3)+(q**4)-(2*(p**2))
+if (l>680):
+    print(l)
+    print("P y Q satisfacen la expresión")
+else:
+    print(l)
+    print("P y Q no satisfacen la expresión")
