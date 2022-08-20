b46207a2af5dfd4209467b54338a934245406374
Alejandro Parraga <japarra00115@universidadean.edu.co>
2022-08-20 10:47:24 -0500

    Ejercicio_4

--- Taller_Estructura_Control_Repeticion/Ejercicio_4.py
+++ Taller_Estructura_Control_Repeticion/Ejercicio_4.py
@@ -3,12 +3,13 @@ from re import A
 
 c=0
 a=6
+suma=0
 while(c<=12):    
     a+=5
     c+=1    
 
-for i in range(6,a):
-    i+=5
+for i in range(6,a,5):
+        suma+=i
 
 print (a)
-print(i)
+print(suma)
