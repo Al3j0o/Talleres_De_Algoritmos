20be8a6609b9c857a9d495f849455da5cd4b1ac1
Alejandro Parraga <japarra00115@universidadean.edu.co>
2022-08-21 21:51:15 -0500

    Ejercicio_10

---
+++ Taller_Estructura_Control_Repeticion/ejercicio_10.py
@@ -0,0 +1,13 @@
+n=int(input("Ingrese el numero de estudiantes: "))
+x=1
+alt=-10
+while(x<=n):
+    alt1=float(input("Ingrese la altura: "))
+    x+=1
+    if(alt1>alt):
+        alt=alt1
+print("el mas alto es:",alt)
+    
+
+
+
