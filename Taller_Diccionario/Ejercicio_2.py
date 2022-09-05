4db44d39ac93402754c2b1bafebdffdfb4ed7f73
lauraholguin0 <japarra00115@universidadean.edu.co>
2022-09-05 11:40:07 -0500

    Ejercicio_2

---
+++ Taller_Diccionario/Ejercicio_2.py
@@ -0,0 +1,6 @@
+Diccionario= {'Mikel': 3, 'Ane': 8, 'Amaia': 12, 'Unai': 5, 'Jon': 8, 'Ainhoa': 7, 'Maite': 5} 
+lista=[]
+for key in Diccionario:
+    lista.append(Diccionario[key])
+
+print(lista)
