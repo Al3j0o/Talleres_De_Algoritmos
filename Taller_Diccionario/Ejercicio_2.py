9bb5a1cecce0e7f464fc0d828cbbed6d23681ae9
lauraholguin0 <japarra00115@universidadean.edu.co>
2022-09-05 11:53:58 -0500

    Ejercicio_2

--- Taller_Diccionario/Ejercicio_2.py
+++ Taller_Diccionario/Ejercicio_2.py
@@ -1,6 +1,6 @@
 Diccionario= {'Mikel': 3, 'Ane': 8, 'Amaia': 12, 'Unai': 5, 'Jon': 8, 'Ainhoa': 7, 'Maite': 5} 
 lista=[]
-for key in Diccionario:
-    lista.append(Diccionario[key])
-
+for i in Diccionario:
+    if (not(Diccionario[i] in lista)):
+        lista.append(Diccionario[i])
 print(lista)
